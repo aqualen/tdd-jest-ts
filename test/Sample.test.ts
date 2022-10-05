2a60a8e5c496f1889f7b9fb35d8f0ded50ceba41
aqualen <leonard@nori.com>
2022-10-04 17:16:26 -0700

    A real failing test

--- test/Sample.test.ts
+++ test/Sample.test.ts
@@ -1,3 +1,10 @@
-test("Initial test", () => {
-  expect(2).toEqual(3);
+import { Sample } from "../src/Sample";
+
+describe("Sample test suite", () => {
+
+  test("hello world output", () => {
+    let s= new Sample();
+    expect(s.hello("Ivan")).toEqual("Hello Mom");
+  });
 });
+
